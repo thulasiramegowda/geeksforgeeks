0f31244b8c578c28b038953cd75a073ee27e1cda
Thulasi G T <thulasiramegowda29@gmail.com>
2026-09-23 18:59:42 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/The Else if Statement/the-else-if-statement.c
@@ -0,0 +1,13 @@
+void utility(int number) {
+    // Write the if, else if, else statements below
+    
+    if(number>100){
+        printf("Big\n");
+    }
+    else if(number<10){
+        printf("Small\n");
+    }
+    else{
+        printf("Number\n");
+    }
+}
